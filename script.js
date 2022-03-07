5c2b2072bb658aaada5250f8483276bbc48ea3fa
unknown <you@example.com>
2022-03-07 15:59:00 +0200

    added button

---
+++ script.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('.btn')
+const text = document.querySelector('.text')
+let shown = true;
+
+btn.onclick = () => {
+    if(shown){
+        btn.innerText = 'Show Text';
+        text.style.opacity = 0
+    } else {
+        btn.innerText = 'Hide Text'
+        text.style.opacity = 1;
+    }
+    shown = !shown
+}
